bc1297514bac6300894030663191f34c4bf6869b
Damian Bemben <bembendamian@yahoo.co.uk>
2019-04-17 00:01:55 +0100

    Improvements on readability

--- public/javascripts/mapping.js
+++ public/javascripts/mapping.js
@@ -4,7 +4,6 @@ const data_values = [];
 $(document).ready(() => {
     var items = [];
 
-
 var json = $.getJSON('http://api.luftdaten.info/static/v2/data.24h.json', function(data){
     var counter = 0
 
@@ -22,6 +21,11 @@ var json = $.getJSON('http://api.luftdaten.info/static/v2/data.24h.json', functi
         minZoom: 12
         // maxZoom: 10
     })
+    navigator.geolocation.getCurrentPosition(function(location) {
+        var userlocation = new L.LatLng(location.coords.latitude, location.coords.longitude);
+        var marker = L.marker(userlocation).addTo(mymap);
+    });
+
     mymap.setView([53.382, -1.47], 13);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Sensor Data <a href="https://luftdaten.info/en/home-en/">Luftdaten</a> | Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -31,28 +35,42 @@ var json = $.getJSON('http://api.luftdaten.info/static/v2/data.24h.json', functi
     }).addTo(mymap);
     console.log(items)
     circles = []
+    var light = "#FFF275"
+    var medium = "#FF8C42"
+    var high = '#FF3C38'
+    var veryhigh = '#d600a4'
+    var danger =  '#a20049'
+    var bigdanger = '#1a0006'
+    var safe =  '#6699CC'
     for (var i = 0; i < items.length; i++) {
         console.log(items[i][3])
-        if (items[i][3] >= 20 || items[i][4] >= 10){
-            colour = '#FDE74C'
+        console.log(items[i][4])
+        if (items[i][3] >= 20 && items[i][3] < 30|| items[i][4] >= 10 && items[i][4] < 15){
+            colour = light
         }
-        else if (items[i][3] >= 30 || items[i][4] >= 15){
-            colour = '#FA7921'
+        else if (items[i][3] >= 30 && items[i][3] < 50|| items[i][4] >= 15 && items[i][4] < 25) {
+            colour = medium
         }
-        else if (items[i][3] >= 50 || items[i][4] >= 25){
-            colour = '#E55934'
+        else if (items[i][3] >= 50 && items[i][3] < 70 || items[i][4] >= 25 && items[i][4] < 35){
+            colour = high
         }
         else if (items[i][3] >= 70 || items[i][4] >= 35){
-            colour = '#D62839'
+            colour = veryhigh
+        }
+        else if (items[i][3] >= 100 || items[i][4] >= 50){
+            colour =  danger
+        }
+        else if (items[i][3] >= 150 || items[i][4] >= 75){
+            colour = bigdanger
         }
         else {
-            colour ='#4392F1'
+            colour = safe
         }
         circles.push(L.circle([items[i][1], items[i][2]], {
             color: 'black',
             fillColor: colour,
-            fillOpacity: 0.4,
-            radius: 225,
+            fillOpacity: 0.8,
+            radius: 200,
             p10: [items[i][3]],
             p2: [items[i][4]],
             choice_id: i
@@ -79,49 +97,97 @@ var json = $.getJSON('http://api.luftdaten.info/static/v2/data.24h.json', functi
 
     var ctx = document.getElementById("myChart").getContext('2d');
     var myChart = new Chart(ctx, {
-        type: 'line',
+        type: 'bar',
+
         data: {
-            labels: ["8am", "12am", "4pm", "8pm", "12pm"],
-            datasets: [{
-                label: 'CO2 Level (%)',
-                data: [18, 19, 12, 13, 5],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                ],
-                borderWidth: 1
-            },
+            labels: ["pm10", "pm2.5"],
+            datasets: [
                 {
-                    label:"National Average",
-                    data: [17, 16, 10, 10, 1],
+                    label: 'Within guidelines',
+                    data: [20, 10],
                     backgroundColor: [
-                        'rgba(108, 219, 10, 0.5 )',
+                        safe,
+                        safe
                     ],
-                    borderColor: [
-                        'rgba(108, 219, 10, 1)',
-                    ],
+
                     borderWidth: 1
                 },
                 {
-                    label:"WHO 24 Hour Guidelines PM2.5",
-                    data: [25, 25, 25, 25, 25],
+                label: '3% Increase In LTM',
+                data: [30, 15],
+                backgroundColor: [
+                    light,
+                    light,
+                ],
+
+                borderWidth: 1
+            },
+                {
+                    label: '9% Increase In LTM',
+                    data: [50, 25],
                     backgroundColor: [
-                        'rgba(108, 219, 10, 0.5 )',
+                        medium,
+                        medium
                     ],
-                    borderColor: [
-                        'rgba(108, 219, 10, 1)',
-                    ],
+
                     borderWidth: 1
-                }]
+                }
+                ,
+                {
+                    label: '15% Increase in LTM',
+                    data: [75, 37.5],
+                    backgroundColor: [
+                        high,
+                        high
+                    ],
+
+                    borderWidth: 1
+                },
+                {
+                    label: '1.2% Increase In STM, 15% Increase in LTM',
+                    data: [100, 50],
+                    backgroundColor: [
+                        veryhigh,
+                        veryhigh
+                    ],
+
+                    borderWidth: 1
+                },
+                {
+                    label: '2.5% Increase In STM, 15% Increase in LTM',
+                    data: [150, 75],
+                    backgroundColor: [
+                        danger,
+                        danger
+                    ],
+
+                    borderWidth: 1
+                },
+                {
+                    label: '5% Increase In STM, 15% Increase in LTM',
+                    data: [175, 175],
+                    backgroundColor: [
+                        bigdanger,
+                        bigdanger,
+                    ],
+
+                    borderWidth: 1
+                },]
         },
         options: {
+
+            title: {
+                display:true,
+                text:"Effects of PM10 and PM2.5 on Long and Short Term Mortality (LTM, STM)"
+            },
             scales: {
                 yAxes: [{
                     ticks: {
                         beginAtZero:true
                     }
+                }],
+                xAxes: [{
+                    stacked:true
                 }]
             }
         }
